e0258c6223a2bff14995d074350d76cd92b8edbc
Aspect <ttmmiizz101@gmail.com>
2024-10-06 20:05:49 -0400

    Switch to rustls

--- src/main.rs
+++ src/main.rs
@@ -77,7 +77,7 @@ use axum::{
     routing::{get, post},
     Json, Router,
 };
-use axum_server::tls_openssl::OpenSSLConfig;
+use axum_server::tls_rustls::RustlsConfig;
 
 use serde::{Deserialize, Serialize};
 
@@ -388,10 +388,10 @@ async fn main() -> Result<()> {
         });
 
     let config =
-        OpenSSLConfig::from_pem(include_bytes!("../cert.pem"), include_bytes!("../key.pem"))?;
+        RustlsConfig::from_pem(include_bytes!("../cert.pem").to_vec(), include_bytes!("../key.pem").to_vec()).await?;
 
     spawn(async move {
-        axum_server::bind_openssl(SocketAddr::from(([0, 0, 0, 0], port as u16)), config)
+        axum_server::bind_rustls(SocketAddr::from(([0, 0, 0, 0], port as u16)), config)
             .serve(app.into_make_service())
             .await
             .unwrap();
